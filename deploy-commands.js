4df6879a0512f47fdf6f8c60e75d8ba6e05af2b0
devZenta <hugo.ghesquier59@gmail.com>
2025-01-28 10:36:58 +0100

    feat: add initial files for user and admin commands, implementation to follow

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,6 +2,8 @@ const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
 
+require('dotenv').config();
+
 const commands = [];
 // Grab all the command folders from the commands directory you created earlier
 const foldersPath = path.join(__dirname, 'commands');
